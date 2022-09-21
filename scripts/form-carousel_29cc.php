c5f159df92ac655631834616741f96c5d6816f2d
karthik-mohan <68692486+karthik-mohan@users.noreply.github.com>
2022-09-21 08:45:57 -0400

    change comitted

---
+++ scripts/form-carousel_29cc.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'info@abili-tech.com',
+    'to' => 'info@abili-tech.com'
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'name' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 3,
+    'type' => 'tel',
+    'label' => 'Phone',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Phone\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 4,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
